4992242aec1e73dfbde180497bc63a54cf6055c7
mainlee <simao.anderson4@gmail.com>
2020-05-11 15:29:51 -0300

    Adição do algoritmo de Fila Circular

--- public/js/fila/algo.js
+++ public/js/fila/algo.js
@@ -1,19 +1,45 @@
 //Vetor com tamanho fixo
-const vetor = [tam];
+const vetor = [];
 
 //Variaveis de inicio e fim
-var i, f;
+var ini = 0;
+var fim = 0;
 
-//Atualiza posições
-function atualizaPos(){
-    i = vetor[0];
-    f = vetor[vetor.length-1];
+function criaVetor(tam){
+    //vetor = [];
+    for(var i = 0; i<tam; i++){
+        vetor[i] = undefined;
+    }
 }
 
-function insere(num){
-    //Verifca se a fila está fazia
-    if(vetor.length == 0){
-        vetor.push(num);
+function filaVazia(){
+    if(ini == fim){
+        console.log('Teste');
+        alert('A fila está vazia!');
+        return true;
+    }
+    return false;
+}
+
+function enfileira(num){
+    var tam = vetor.length;
+    //Verifica o overflow da fila
+    if((fim+1)%tam == ini){
+        alert('Fila cheia! Por favor desenfileire algum número!');
+    } else {
+        //Insere o valor na última posição
+        vetor[fim] = num;
+        fim = (fim+1)%tam;
+    }
+}
+
+function desenfileira(){
+    var tam = vetor.length;
+    //Verifica se a fila está fazia
+    if(!filaVazia()){
+        //Apagando o valor da posição inicial da fila
+        vetor[ini] = undefined;
+        ini = (ini+1)%tam;
     }
 }
 
