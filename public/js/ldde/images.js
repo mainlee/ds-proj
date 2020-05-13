6547c084117f65c9ec6067f05a2a55342f56e424
mainlee <simao.anderson4@gmail.com>
2020-05-13 01:57:25 -0300

    Organização do projeto e adição de imagens para fila

--- public/js/ldde/images.js
+++ public/js/ldde/images.js
@@ -1,13 +1,13 @@
 //Imagens dos nós
 
 var fNo = new Image();
-fNo.src = "../public/img/firstNo.png";
+fNo.src = "../public/img/ldde/firstNo.png";
 
 var fNo_p = new Image();
-fNo_p.src = "../public/img/fisrtNoWN.png";
+fNo_p.src = "../public/img/ldde/fisrtNoWN.png";
 
 var no_dl = new Image();
-no_dl.src = "../public/img/noDL.png";
+no_dl.src = "../public/img/ldde/noDL.png";
 
 var lastNo = new Image();
-lastNo.src = "../public/img/lastNo.png";
+lastNo.src = "../public/img/ldde/lastNo.png";
