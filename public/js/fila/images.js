5be12498c571872a2f76b95fb51015875eeffb36
mainlee <simao.anderson4@gmail.com>
2020-05-13 02:36:58 -0300

    Rederização da fila após determinar o tamanho

---
+++ public/js/fila/images.js
@@ -0,0 +1,19 @@
+//Imagens da fila
+
+var fila = new Image();
+fila.src = "../public/img/fila/fila0.png"
+
+var fila4 = new Image();
+fila4.src = "../public/img/fila/fila4.png"
+
+var fila5 = new Image();
+fila5.src = "../public/img/fila/fila5.png"
+
+var fila6 = new Image();
+fila6.src = "../public/img/fila/fila6.png"
+
+var fila7 = new Image();
+fila7.src = "../public/img/fila/fila7.png"
+
+var fila8 = new Image();
+fila8.src = "../public/img/fila/fila8.png"
--- public/js/fila/render.js
+++ public/js/fila/render.js
@@ -1,5 +1,66 @@
+//=============================================================//
+//                        INICIALIZAÇÃO                        //
+//=============================================================//
+const canvas = document.getElementById('fila');
+const ctx = canvas.getContext('2d');
+
+ctx.lineWidth = 2;
+
+//variaveis de posicao e tamanho
+//tamanho do canvas
+const canvas_h = 570;
+const canvas_w = 1100;
+
+//Variavel de tamanho do vetor
+var tamanho;
+
+//Função para determinar tamanho da fila
 function criou(){
-    document.getElementById("btn-view").hidden = false;
-    document.getElementById("valor").value = "";
-    document.getElementById("cria").hidden = true
+    tamanho = document.getElementById("valor").value;
+    tamanho = Number(tamanho);
+    if(tamanho < 4 || tamanho > 8){
+        alert('Favor preencher com números entre 4 e 8');
+        document.getElementById("valor").value = "";
+    } else {
+        document.getElementById("btn-view").hidden = false;
+        document.getElementById("valor").value = "";
+        document.getElementById("input-txt").innerHTML = "Insira um valor:";
+        document.getElementById("cria").hidden = true;      
+        limpaTela();
+        renderFila();
+    }
 }
+
+function limpaTela(){
+    ctx.fillStyle = "#249978";
+    ctx.fillRect(0, 0, canvas_w, canvas_h);    
+}
+
+function renderEmpty(){
+    ctx.drawImage(fila, 250 , 50);
+}
+
+function renderFila(){
+    if(tamanho == 4){
+        fila.src = "../public/img/fila/fila4.png";
+        ctx.drawImage(fila4, 250 , 50);
+    }
+    else if (tamanho == 5){
+        fila.src = "../public/img/fila/fila5.png";
+        ctx.drawImage(fila5, 250 , 50);
+    }
+    else if (tamanho == 6){
+        fila.src = "../public/img/fila/fila6.png";
+        ctx.drawImage(fila6, 250 , 50);
+    }
+    else if (tamanho == 7){
+        fila.src = "../public/img/fila/fila7.png";
+        ctx.drawImage(fila7, 250 , 50);
+    } else {
+        fila.src = "../public/img/fila/fila8.png";
+        ctx.drawImage(fila8, 250 , 50);
+    }
+}
+
+limpaTela();
+renderEmpty();
